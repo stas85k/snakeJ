1c930a435ae69e124493f1508e7f25032d593adf
jenkins <stas85k@gmail.com>
2016-03-10 13:33:00 +0200

    Commit1

---
+++ src/Snake.java
@@ -0,0 +1,30 @@
+/**
+ * Created by stas on 10/03/2016.
+ */
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
