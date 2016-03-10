68a93f48ae0f8be7b37c719f649525775a1a6575
jenkins <stas85k@gmail.com>
2016-03-10 13:44:15 +0200

    Commit2

--- src/Board.java
+++ src/Board.java
@@ -11,10 +11,10 @@ import java.awt.event.KeyEvent;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 300;
-    private final int B_HEIGHT = 300;
+    private final int B_WIDTH = 600;
+    private final int B_HEIGHT = 600;
     private final int DOT_SIZE = 10;
-    private final int ALL_DOTS = 900;
+    private final int ALL_DOTS = 3600;
     private final int RAND_POS = 29;
     private final int DELAY = 140;
 
